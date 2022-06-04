2a861074c0c431db2161f61298e23b0356e7e8e2
iaiapps <ikromudin.ai@gmail.com>
2022-06-04 13:05:45 +0700

    add fungsi verifikasi

--- src/components/ModalVerIjazah.vue
+++ src/components/ModalVerIjazah.vue
@@ -1,28 +1,78 @@
 <template>
-    <div class="modal fade" id="ModalIjazah" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div
+        class="modal fade"
+        id="ModalIjazah"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+    >
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title" id="exampleModalLabel">Edit Identitas</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <button
+                        type="button"
+                        class="btn-close"
+                        data-bs-dismiss="modal"
+                        aria-label="Close"
+                    ></button>
                 </div>
                 <div class="modal-body text-center">
                     <h4>Apakah anda yakin ingin memverifikasi identitas pada Ijazah?</h4>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-                        Tidak
-                    </button>
-                    <button type="button" class="btn btn-primary">Setuju</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Tidak</button>
+                    <button
+                        type="button"
+                        class="btn btn-primary"
+                        v-on:click="verIjazah"
+                        data-bs-dismiss="modal"
+                    >Setuju</button>
                 </div>
             </div>
         </div>
     </div>
-
 </template>
 
 <script>
+import axios from "axios"
+
 export default {
-    name: "ModalVerIjazah"
+    name: "ModalVerIjazah",
+    props: [
+        "dataVerified",
+        "url"
+    ],
+    emits: [
+        "buttoncobaver"
+    ],
+    data() {
+        return {
+            token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMjAzNjA4Mjc2MGI5OTUwMTBiOWEwYTkzOTUwMDc2NTFjZjA4ODU3OWJkZTUwMjI1ZTQyYjg5MDU1ZGJkY2UzNDYzZDEwMWJhYjgzNTMzOGUiLCJpYXQiOjE2NTQyNDE5NDkuMjQ2OTE4OTE2NzAyMjcwNTA3ODEyNSwibmJmIjoxNjU0MjQxOTQ5LjI0NjkyMjk2OTgxODExNTIzNDM3NSwiZXhwIjoxNjg1Nzc3OTQ5LjE3NTg0NzA1MzUyNzgzMjAzMTI1LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.vurHQdr2foqKVx-Qvtavo7EcXz2892O2faarCRB3C7ATYSB1Pyy1lnaR0IlQnwtvO1Ki23VA2K_w47uni87PKfjeC-Wbodm8qp_J4ybF_L6EMwvh4x6ruSFSqYsZRWEgfdSQW_aS1oq8CIJ9GvHaXI98AZbCUF5tDjOpDfIFc04-YOcOKIuhdUn6TwOcKFZccelRglPKXKRp6Enr0s2fH2OgSYRBivNZ8xRhODF4fNQLXG96g_SLC0ubQqbFzOdegN5y0PFhGgkoRYM5w9Ou7Ji4tl8bNNFf9JkDDFWyz3pHip7Sh0-qUkhWQMo6EkohIQqbLAl7X4fntN9waklPFwGlcbPLnmqaHv-HUmceKGeEprYKA8PjUD59skH5mE8Ccl4j4alPfwtdrtb9cPKWvNmZKzzl_RoNGCEcNaLNolgx1GcT4CdtrGiLTpdtcqjoX9SvabVO0AV9xdE0s65dKgNT-4b8qn1H81ZNZAWAbYYVJkg5cdktgNHon6xjpdYFE6i0wKub3sgVA9_EXBXJd5_yCTn2Sq2qQMm5o-npmorSmuKaxm8y1wCRoPS77ceG6ChpnEzS94QnzFMO7Zi6cPQF-YXMa0ejbfRbdnA1RxC-PhkuuyzZ-G-XeoAFR0yAajxMqcx1P71yG1-tZYJH-ExeMVRxItrXSSdxsfvo-Xc",
+            verified: "",
+            routeIndex: this.$route.params.id
+        }
+    },
+    methods: {
+        isVerified() {
+            axios.get(`${this.url}/${this.routeIndex}/verified`, {
+                headers: {
+                    "Authorization": `Bearer ${this.token}`,
+                }
+            }).then((result) => {
+                const results = result.data.data.isVerified
+                this.verified = results
+                console.log(results)
+            }).catch((err) => { console.log(err) })
+        },
+        verIjazah() {
+            this.isVerified(),
+            this.$emit("clickVerified")
+    
+        },
+        reRender() {
+            console.log("s")
+        }
+    }
 }
 </script>
--- src/views/IjazahPage.vue
+++ src/views/IjazahPage.vue
@@ -1,4 +1,4 @@
-<template>
+<template >
     <HeaderComp></HeaderComp>
     <div class="container pt-3">
         <div class="row mb-3">
@@ -29,16 +29,17 @@
                     >Edit Identitas</button>
                 </div>
                 <small class="mt-3 d-block">*scroll/sentuh gambar untuk menggeser</small>
+            <router-link class="btn btn-primary w-100 mt-3" v-if="isAdmin == true" v-bind:to="{name:'AdminPage'}" >kembali ke halaman admin</router-link>
             </div>
-          
+
             <div class="col-12 col-lg-8">
                 <div class="card p-3 mb-3">
                     <h4 class>Nama Siswa : {{ student.name }}</h4>
                 </div>
-                
+
                 <div class="card p-3 mb-3">
-                    <h4 class v-if="student.isVerified!=null">Status : {{ student.isVerified }}</h4>
-                    <h4 class v-else>Status : Belum Terverifikasi</h4>
+                    <h4 v-if="student.isVerified == true">Status : Sudah Terverifikasi</h4>
+                    <h4 v-else>Status : Belum Terverifikasi</h4>
                 </div>
                 <div class="card p-3 cardijazah">
                     <img class="imgijazah" src="../assets/img/ijazah.png" alt="ijazah" />
@@ -47,7 +48,9 @@
                     <p class="textijazah textkab">Jember</p>
                     <p class="textijazah textprov">Jawa Timur</p>
                     <p class="textijazah textnamasiswa">{{ student.name }}</p>
-                    <p class="textijazah textttl">{{ student.place_of_birth }}, {{ student.date_of_birth }}</p>
+                    <p
+                        class="textijazah textttl"
+                    >{{ student.place_of_birth }}, {{ student.date_of_birth }}</p>
                     <p class="textijazah textnamaortu">{{ student.parents_name }}</p>
                     <p class="textijazah textnis">{{ student.nis }}</p>
                     <p class="textijazah textnisn">{{ student.nisn }}</p>
@@ -57,10 +60,10 @@
             </div>
         </div>
     </div>
-    
+    <!-- <button v-on:click="tombolverified">verrrr</button> -->
     <FooterComp></FooterComp>
     <ModalEditComp></ModalEditComp>
-    <ModalVerIjazah></ModalVerIjazah>
+    <ModalVerIjazah v-bind:url="url" v-on:clickVerified="tombolverified"></ModalVerIjazah>
 </template>
 
 <script>
@@ -84,7 +87,7 @@ export default {
             url: "http://127.0.0.1:8000/api/student",
             token: "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMjAzNjA4Mjc2MGI5OTUwMTBiOWEwYTkzOTUwMDc2NTFjZjA4ODU3OWJkZTUwMjI1ZTQyYjg5MDU1ZGJkY2UzNDYzZDEwMWJhYjgzNTMzOGUiLCJpYXQiOjE2NTQyNDE5NDkuMjQ2OTE4OTE2NzAyMjcwNTA3ODEyNSwibmJmIjoxNjU0MjQxOTQ5LjI0NjkyMjk2OTgxODExNTIzNDM3NSwiZXhwIjoxNjg1Nzc3OTQ5LjE3NTg0NzA1MzUyNzgzMjAzMTI1LCJzdWIiOiIxIiwic2NvcGVzIjpbXX0.vurHQdr2foqKVx-Qvtavo7EcXz2892O2faarCRB3C7ATYSB1Pyy1lnaR0IlQnwtvO1Ki23VA2K_w47uni87PKfjeC-Wbodm8qp_J4ybF_L6EMwvh4x6ruSFSqYsZRWEgfdSQW_aS1oq8CIJ9GvHaXI98AZbCUF5tDjOpDfIFc04-YOcOKIuhdUn6TwOcKFZccelRglPKXKRp6Enr0s2fH2OgSYRBivNZ8xRhODF4fNQLXG96g_SLC0ubQqbFzOdegN5y0PFhGgkoRYM5w9Ou7Ji4tl8bNNFf9JkDDFWyz3pHip7Sh0-qUkhWQMo6EkohIQqbLAl7X4fntN9waklPFwGlcbPLnmqaHv-HUmceKGeEprYKA8PjUD59skH5mE8Ccl4j4alPfwtdrtb9cPKWvNmZKzzl_RoNGCEcNaLNolgx1GcT4CdtrGiLTpdtcqjoX9SvabVO0AV9xdE0s65dKgNT-4b8qn1H81ZNZAWAbYYVJkg5cdktgNHon6xjpdYFE6i0wKub3sgVA9_EXBXJd5_yCTn2Sq2qQMm5o-npmorSmuKaxm8y1wCRoPS77ceG6ChpnEzS94QnzFMO7Zi6cPQF-YXMa0ejbfRbdnA1RxC-PhkuuyzZ-G-XeoAFR0yAajxMqcx1P71yG1-tZYJH-ExeMVRxItrXSSdxsfvo-Xc",
             student: "",
-            routeIndex:this.$route.params.id
+            routeIndex: this.$route.params.id,
         }
     },
     methods: {
@@ -93,20 +96,23 @@ export default {
                 headers: {
                     "Authorization": `Bearer ${this.token}`,
                 }
-            }).then((result)=>{
+            }).then((result) => {
                 const results = result.data.data;
                 this.student = results;
                 console.log(results);
-            }).catch((err)=>{
+            }).catch((err) => {
                 console.log(err)
             })
-        }
-     
+        },
+        tombolverified() {
+            this.student.isVerified = true
+        },
+
     },
-    mounted(){
-        setTimeout(()=>{
+    mounted() {
+        setTimeout(() => {
             this.getDataIjazah()
-        },500)
+        }, 500)
     }
 }
 </script>
